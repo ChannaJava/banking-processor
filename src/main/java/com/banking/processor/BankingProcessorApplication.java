4d38dbf93e3deec1e5dbb86062eeaf34b2832462
channuek-dev <channuek@gmail.com>
2026-08-12 17:19:54 +0530

    Add startup class and properties file

---
+++ src/main/java/com/banking/processor/BankingProcessorApplication.java
@@ -0,0 +1,11 @@
+package com.banking.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProcessorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingProcessorApplication.class, args);
+    }
+}
